d2d0525ba8c58fa4f0f134bdfc3873b4664bbd51
archiehonorio <archiehonorio404>
2020-02-22 00:24:53 +0800

    new

--- ARCHIE.py
+++ ARCHIE.py
@@ -6,3 +6,34 @@ def countdown():
         time.sleep(1)
 countdown()
 print("YOU HAVE BEEN HACKED")
+
+# Program make a simple calculator
+
+# This function adds two numbers 
+def add(x, y):
+   return x + y
+
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+   return x * y
+
+# This function divides two numbers
+def divide(x, y):
+   return x / y
+
+print("Select operation.")
+print("2.Subtract")
+
+
+# Take input from the user 
+choice = input("Enter 2: ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
